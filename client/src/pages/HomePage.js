c150d41d94b3d02db54b6477461e7e3f22b59f8e
Sandro Hallado <sandrohallado@yahoo.com>
2020-07-29 19:14:40 -0700

    Add home page

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const HomePage = () => (
+  <div>
+    <p>VLG Logo</p>
+    <button>Sign In</button>
+    <h1>Video Game Logic</h1>
+    <p>Your one stop shop for video game info old, new, and upcoming</p>
+    <button>Sign Up</button>
+  </div>
+);
+
+export default HomePage;
