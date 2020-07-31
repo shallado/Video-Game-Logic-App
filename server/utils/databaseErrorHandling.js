37cb1efd91c54feb7494dc9cf1e8c0a8f7516366
Sandro Hallado <sandrohallado@yahoo.com>
2020-07-31 11:30:00 -0700

    Create a centralize area to handle error messages

---
+++ server/utils/databaseErrorHandling.js
@@ -0,0 +1,51 @@
+const httpStatusCodes = require('./statusCodes');
+
+const userErrorHandling = (err) => {
+  const formField = err.keyValue && Object.keys(err.keyValue);
+  let setError;
+
+  if (err.code === 11000) {
+    setError = {
+      httpStatus: httpStatusCodes.BAD_REQUEST,
+      description: `${formField} is already taken`,
+    };
+  } else if (err.code === 121) {
+    setError = {
+      httpStatus: httpStatusCodes.BAD_REQUEST,
+      description: `document failed validation`,
+    };
+  } else if (err.httpStatus === 400) {
+    setError = err;
+  } else if (err.msg === '400') {
+    setError = {
+      httpStatus: httpStatusCodes.UNAUTHORIZED,
+      description: 'unable to verify city and zipcode',
+    };
+  } else if (err.msg === '401') {
+    setError = {
+      httpStatus: httpStatusCodes.UNAUTHORIZED,
+      description: 'invalid password',
+    };
+  } else if (err.httpStatus === 404) {
+    setError = err;
+  } else if (err.number === 404) {
+    setError = {
+      httpStatus: err.number,
+      description: err.message,
+    };
+  } else if (err.msg === '404') {
+    setError = {
+      httpStatus: httpStatusCodes.NOT_FOUND,
+      description: 'unable to find user',
+    };
+  } else {
+    setError = {
+      httpStatus: httpStatusCodes.INTERNAL_SERVER,
+      description: err.message,
+    };
+  }
+
+  return setError;
+};
+
+module.exports = userErrorHandling;
