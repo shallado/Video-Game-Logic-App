d55842ae20b4a627ec97762b4fc7723f7ec62deb
Sandro Hallado <sandrohallado@yahoo.com>
2020-08-03 10:30:00 -0700

    Create password utility to handle password related processing

---
+++ server/utils/password.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt');
+const APIError = require('./apiError');
+const httpStatusCodes = require('./statusCodes');
+
+// hashes user created passwords
+const hashPassword = (password) => {
+  const saltRounds = 12;
+  const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+  if (!password.match(regex)) {
+    // must be 8 to 15 characters long
+    // contain one lowercase letter, one uppercase letter, one numeric digit and one special character
+    throw new APIError(
+      'Bad Request',
+      httpStatusCodes.BAD_REQUEST,
+      'password does not meet requirements try again'
+    );
+  } else {
+    return bcrypt.hash(password, saltRounds);
+  }
+};
+
+// compares user password in database and user input password
+const comparePassword = (inputPassword, databasePassword) => {
+  return bcrypt.compare(inputPassword, databasePassword).then((isValid) => {
+    if (!isValid) {
+      return '401';
+    }
+  });
+};
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+};
