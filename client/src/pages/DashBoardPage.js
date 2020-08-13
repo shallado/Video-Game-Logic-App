979d63211c1181d316e189222911ab49d253266b
Sandro Hallado <sandrohallado@yahoo.com>
2020-08-13 10:30:00 -0700

    Add a dashboard page

---
+++ client/src/pages/DashBoardPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DashBoardPage = () => (
+  <div>
+    <h1>DashBoardPage</h1>
+  </div>
+);
+
+export default DashBoardPage;
--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import DashBoardPage from '../pages/DashBoardPage';
 import HomePage from '../pages/HomePage';
 import SignUpPage from '../pages/SignUpPage';
 
@@ -7,6 +8,7 @@ const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route path={'/'} component={HomePage} exact={true} />
+      <Route path={'/dashboard'} component={DashBoardPage} />
       <Route path={'/signup'} component={SignUpPage} />
     </Switch>
   </BrowserRouter>
