5df3420f333f0400d9cb2c71a2f5ee10c3a6f7f1
Sandro Hallado <sandrohallado@yahoo.com>
2020-08-15 10:30:00 -0700

    Move location validation to only the backend

--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -1,14 +1,20 @@
-const locationValidator = require('@vgl/common');
+const axios = require('axios');
 const { body } = require('express-validator');
+const { apiKey } = require('../config/mapbox');
 
 // checks to see if the provided location info city and zipcode are valid
-exports.locationCheck = body(['city']).custom((value, { req }) =>
-  locationValidator({ city: value, zipcode: req.body.zipcode }).then((err) => {
-    if (err) {
+exports.locationCheck = body(['city']).custom((value, { req }) => {
+  const city = value;
+  const request = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}, ${req.body.zipcode}.json?access_token=${apiKey}`;
+
+  return axios.get(request).then((response) => {
+    const placeType = response.data.features[0].place_type[0];
+    // verify to make sure that city and zipcode inputs are valid locations
+    if (placeType !== 'postcode') {
       throw new Error('400');
     }
-  })
-);
+  });
+});
 
 // checks to make sure that input data are valid data types and follow input guidelines
 exports.inputValidation = () => {
--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const path = require('path');
-require('module-alias/register');
 const authRouter = require('./routes/auth');
 const igdbRouter = require('./routes/igdb');
 const reviewRouter = require('./routes/review');
 const userRouter = require('./routes/user');
 const videoGameRouter = require('./routes/videoGame');
-require('dotenv').config({ path: path.join(__dirname, '../client/.env') });
 require('./models');
 
 const app = express();
