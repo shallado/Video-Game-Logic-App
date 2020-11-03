a199142828e1b639c5705007edfb2463d0703ab0
Sandro Hallado <sandrohallado@yahoo.com>
2020-11-02 21:57:36 -0800

    Change logic on hovering video game card and logic for video game category

--- client/src/components/VideoGameCard.js
+++ client/src/components/VideoGameCard.js
@@ -1,75 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import AddRemoveVideoGame from './AddRemoveVideoGame';
-import MoreInfoModal from './MoreInfoModal';
-import ScreenShotCarousel from './ScreenShotCarousel';
-import PlayOptionsModal from './PlayOptionsModal';
+import IconRightArrowTwo from '../svgs/IconRightArrowTwo';
 import { setCurrentGame } from '../actions/game';
+import { showModal } from '../actions/modal';
 
 class VideoGameCard extends Component {
-  state = {
-    hover: false,
-  };
-
   handleMouseEnter = () => {
-    this.setState(() => ({ hover: true }));
-    this.props.setCurrentGame(this.props.gameInfo);
+    if (this.props.gameInfo.id !== this.props.currentGame.id) {
+      this.props.setCurrentGame(this.props.gameInfo);
+    }
   };
 
-  handleMouseLeave = () => {
-    this.setState(() => ({ hover: false }));
+  handleShowModal = () => {
+    this.props.showModal();
   };
 
   render() {
     return (
-      <div className="video-game-card">
-        {/* {this.state.hover ? (
-          <div onMouseLeave={this.handleMouseLeave}>
-            <div>
-              <ScreenShotCarousel gameInfo={this.props.gameInfo} />
-            </div>
-            <div>
-              <div>
-                <PlayOptionsModal gameInfo={this.props.gameInfo} />
-                <AddRemoveVideoGame />
-                <MoreInfoModal gameInfo={this.props.gameInfo} />
-              </div>
-              <div>
-                {this.props.gameInfo.age_ratings && (
-                  <div>
-                    <p>Rated</p>
-                    {this.props.gameInfo.age_ratings.map(({ rating, id }) => (
-                      <p key={id}>{rating}</p>
-                    ))}
-                  </div>
-                )}
-                <div>
-                  <p>Genre</p>
-                  {this.props.gameInfo.genres.map(({ name, id }) => (
-                    <p key={id}>{name}</p>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <img
-            onMouseEnter={this.handleMouseEnter}
-            src={this.props.gameInfo.cover.url.replace('thumb', 'logo_med')}
-          />
-        )} */}
+      <div className="video-game-card" onMouseEnter={this.handleMouseEnter}>
         <img
-          onMouseEnter={this.handleMouseEnter}
           src={this.props.gameInfo.cover.url.replace('thumb', 'cover_big')}
           className="video-game-card__img"
         />
+        <div className="video-game-card__overlay">
+          <div
+            className="video-game-card__icon-container"
+            onClick={this.handleShowModal}
+          >
+            <IconRightArrowTwo />
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentGame: (currentGame) => dispatch(setCurrentGame(currentGame)),
+const mapStateToProps = (state) => ({
+  currentGame: state.game.currentGame,
 });
 
-export default connect(null, mapDispatchToProps)(VideoGameCard);
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentGame: (currentGame) => dispatch(setCurrentGame(currentGame)),
+  showModal: () => dispatch(showModal('moreInfoModal')),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(VideoGameCard);
--- client/src/components/VideoGameCategory.js
+++ client/src/components/VideoGameCategory.js
@@ -1,46 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import { connect } from 'react-redux';
 import VideoGameCard from './VideoGameCard';
 import IconRightArrow from '../svgs/IconRightArrow';
 
-class VideoGameCategory extends Component {
-  render() {
-    const items =
-      this.props.categoryGames &&
-      this.props.categoryGames[0][this.props.index].map((game) => (
-        <VideoGameCard gameInfo={game} key={game.id} />
-      ));
-    const responsive = {
-      1140: { items: 5 },
-    };
+const VideoGameCategory = (props) => {
+  const items =
+    props.categoryGames.length > 0 &&
+    props.categoryGames[0][props.index].map((game) => (
+      <VideoGameCard gameInfo={game} key={game.id} />
+    ));
+  const responsive = {
+    1140: { items: 5 },
+  };
 
-    return (
-      <div className="category__container">
-        <div className="category__heading-container">
-          <h2 className="category__heading">{this.props.genre}</h2>
-          <IconRightArrow />
-        </div>
-        <div>
-          {this.props.categoryGames.length === 0 ? (
-            <div>
-              <p>...Loading</p>
-            </div>
-          ) : (
-            <AliceCarousel
-              mouseTracking
-              items={items}
-              responsive={responsive}
-              infinite={true}
-              disableDotsControls={true}
-              paddingLeft={50}
-            />
-          )}
-        </div>
+  return (
+    <div className="category__container">
+      <div className="category__heading-container">
+        <h2 className="category__heading">{props.genre}</h2>
+        <IconRightArrow />
       </div>
-    );
-  }
-}
+      <div className="category__carousel-container">
+        {props.categoryGames.length === 0 ? (
+          <div>
+            <p>...Loading</p>
+          </div>
+        ) : (
+          <AliceCarousel
+            mouseTracking
+            items={items}
+            responsive={responsive}
+            infinite={true}
+            disableDotsControls={true}
+            paddingLeft={70}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   categoryGames: state.game.categoryGames,
