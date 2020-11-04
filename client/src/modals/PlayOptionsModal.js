3751ccd816510defcebf321232a5f43d4385a3db
Sandro Hallado <sandrohallado@yahoo.com>
2020-11-03 22:36:48 -0800

    style play options modal and watch page

--- client/src/modals/PlayOptionsModal.js
+++ client/src/modals/PlayOptionsModal.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import Modal from 'react-modal';
 import { Link } from 'react-router-dom';
 import IconClose from '../svgs/IconClose';
-import IconPlayBtn from '../svgs/IconPlayBtn';
 import { hideModal } from '../actions/modal';
 
 Modal.setAppElement('#root');
@@ -19,26 +18,30 @@ class PlayOptionsModal extends Component {
         isOpen={this.props.modals.includes('playOptionsModal')}
         onRequestClose={this.handleCloseModal}
         className="play-modal"
+        overlayClassName="play-modal__overlay"
       >
         <div className="play-modal__close-icon-container">
           <div onClick={this.handleCloseModal}>
             <IconClose />
           </div>
         </div>
-        <h4>Play Options</h4>
-        {this.props.currentGame.videos === undefined ? (
-          <p>Unavailable Videos</p>
-        ) : (
-          this.props.currentGame.videos.map(({ id, name, video_id }) => (
-            <Link to={`/watch/${video_id}`} key={id}>
-              <p>{name}</p>
-            </Link>
-          ))
-        )}
-        <button className="btn play-modal__btn">
-          <span className="play-modal__btn-text">Play</span>
-          <IconPlayBtn onClick={this.handleCloseModal} />
-        </button>
+        <div>
+          <h5 className="heading-five">Play Options</h5>
+          {this.props.currentGame.videos === undefined ? (
+            <p>Unavailable Videos</p>
+          ) : (
+            this.props.currentGame.videos.map(({ id, name, video_id }) => (
+              <Link
+                to={`/watch/${video_id}`}
+                key={id}
+                onClick={this.handleCloseModal}
+                className="play-modal__play-options"
+              >
+                <p>{name}</p>
+              </Link>
+            ))
+          )}
+        </div>
       </Modal>
     );
   }
--- client/src/pages/WatchPage.js
+++ client/src/pages/WatchPage.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import IconLeftArrow from '../svgs/IconLeftArrow';
 import ReactPlayer from 'react-player/youtube';
 
 const WatchPage = (props) => {
   return (
-    <div>
-      <ReactPlayer
-        url={`https://www.youtube.com/watch?v=${props.match.params.id}`}
-        controls={true}
-      />
-      <Link to="/">
-        <ion-icon name="close"></ion-icon>
-      </Link>
+    <div className="watch">
+      <div className="watch__left-arrow-icon-container">
+        <div>
+          <Link to="/dashboard" className="watch__link">
+            <IconLeftArrow />
+          </Link>
+        </div>
+      </div>
+      <div className="watch__video-player-container">
+        <ReactPlayer
+          url={`https://www.youtube.com/watch?v=${props.match.params.id}`}
+          controls={true}
+        />
+      </div>
     </div>
   );
 };
