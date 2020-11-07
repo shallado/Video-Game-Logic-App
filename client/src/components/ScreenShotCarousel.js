bad44058e12fa625c438690d781f4e04449189ea
Sandro Hallado <sandrohallado@yahoo.com>
2020-11-06 21:38:11 -0800

    display alternative image when the video game screen shots are unavailable

--- client/src/components/ScreenShotCarousel.js
+++ client/src/components/ScreenShotCarousel.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import AliceCarousel from 'react-alice-carousel';
+import IconBroken from '../svgs/IconBroken';
 
 const ScreenShotCarousel = (props) => {
-  const items = props.currentGame.screenshots.map(({ id, url }) => (
-    <img
-      src={url.replace('thumb', 'screenshot_huge')}
-      key={id}
-      className="more-info-modal__screen-shots"
-    />
-  ));
+  const items =
+    props.currentGame.screenshots &&
+    props.currentGame.screenshots.map(({ id, url }) => (
+      <img
+        src={url.replace('thumb', 'screenshot_huge')}
+        key={id}
+        className="more-info-modal__screen-shots"
+      />
+    ));
 
   return (
-    <AliceCarousel
-      items={items}
-      infinite={true}
-      disableDotsControls={true}
-      disableButtonsControls={true}
-      autoPlayInterval={3000}
-      autoPlay={true}
-      autoWidth={true}
-    />
+    <>
+      {props.currentGame.screenshots ? (
+        <AliceCarousel
+          items={items}
+          infinite={true}
+          disableDotsControls={true}
+          disableButtonsControls={true}
+          autoPlayInterval={3000}
+          autoPlay={true}
+          autoWidth={true}
+        />
+      ) : (
+        <div className="more-info-modal__unavailable-screenshots">
+          <p className="more-info-modal__unavailable-screenshots-description">
+            Unavailable Screen Shots
+          </p>
+          <div>
+            <IconBroken />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
